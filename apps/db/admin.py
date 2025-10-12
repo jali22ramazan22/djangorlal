8df3281abac1720fdf60e0162ef8da5fe472b640
Jalil_Ramazan <j_ramazan@kbtu.kz>
2025-10-12 18:54:29 +0500

    Add autoregister script. Make initial migration of db

--- apps/db/admin.py
+++ apps/db/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+import apps.db.models as models_ref
+from utils.register_models import register_models
+
+
+# Register your models here.
+register_models(models_ref, admin.site.register)
