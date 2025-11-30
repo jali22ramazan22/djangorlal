3c1598e4e4e66d5532a8d30a22dca07e6fe73971
Jalil_Ramazan <j_ramazan@kbtu.kz>
2025-11-30 13:59:52 +0500

    Admin register models for course

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from apps.courses.models import Lesson, Course
+
 
 # Register your models here.
+@register(Lesson)
+class LessonAdmin(ModelAdmin):
+    """Admin model for Lesson"""
+
+    ...
+
+
+@register(Course)
+class CourseAdmin(ModelAdmin):
+    """Admin model for Course"""
+
+    ...
